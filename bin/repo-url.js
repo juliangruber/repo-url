1c0f8aa2d03b796657d30de1b3facfd3d6df5836
Julian Gruber <julian@juliangruber.com>
2013-07-12 10:44:10 +0200

    added private url support to bin

--- bin/repo-url.js
+++ bin/repo-url.js
@@ -2,7 +2,15 @@
 
 var repoUrl = require('..');
 
-repoUrl(process.argv[2], function (err, url) {
+var module = process.argv.pop();
+var flag = process.argv.pop();
+
+var get =
+  flag == '--ssh' ? repoUrl.ssh :
+  flag == '--https' ? repoUrl.https :
+  repoUrl;
+
+get(module, function (err, url) {
   if (err) {
     console.error(err);
     process.exit(1);
