4f88e3f163f54b5d9883a7b15c03099bafa34596
Julian Gruber <julian@juliangruber.com>
2013-07-12 10:36:08 +0200

    add ssh and https methods

--- index.js
+++ index.js
@@ -9,9 +9,35 @@ var client = new Client({
 });
 
 function repoUrl (module, fn) {
+  getRepository(module, function (err, repo) {
+    if (err) return fn(err);
+    fn(null, repo.url);
+  });
+}
+
+repoUrl.ssh = function (module, fn) {
+  getRepository(module, function (err, repo) {
+    if (err) return fn(err);
+    fn(null, repo.url
+      .replace('git://github.com/', 'git@github.com:'));
+  });
+};
+
+// git://github.com/juliangruber/repo-url(.git)
+// https://github.com/juliangruber/repo-url.git
+
+repoUrl.https = function (module, fn) {
+  getRepository(module, function (err, repo) {
+    if (err) return fn(err);
+    fn(null, repo.url
+      .replace('git://', 'https://'));
+  });
+};
+
+function getRepository (module, fn) {
   client.get('/' + module, function (err, pkg) {
     if (err) return fn(err);
     if (!pkg.repository) return fn(new Error('no repository field'));
-    fn(null, pkg.repository.url);
+    fn(null, pkg.repository);
   });
 }
