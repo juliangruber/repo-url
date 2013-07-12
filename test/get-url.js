aab0ff0113f4447398414d24f154650e39e4b365
Julian Gruber <julian@juliangruber.com>
2013-07-12 10:29:08 +0200

    fix test count

--- test/get-url.js
+++ test/get-url.js
@@ -2,7 +2,7 @@ var repoUrl = require('..');
 var test = require('tape');
 
 test('get repo url', function (t) {
-  t.plan(3);
+  t.plan(2);
   repoUrl('repo-url', function (err, url) {
     t.error(err);
     t.equal(url, 'git://github.com/juliangruber/repo-url.git');
